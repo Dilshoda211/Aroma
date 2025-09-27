9d66239e551644d7b10b847bb2f0bc1465e7908b
Dilshoda211 <dilshodabegimyunusova@gmail.com>
2025-09-27 15:53:50 +0500

    assa

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import User,Address
+from django.utils.html import format_html
 
-admin.site.register(User)
+
 admin.site.register(Address)
+
+@admin.register(User)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'phone_number', 'image_tag')
+    search_fields = ('username', )
+    # list_filter = ('created_at', 'category')
+    readonly_fields = ('image_tag',)
+    def image_tag(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" width="60" height="60" style="object-fit: cover;" />', obj.photo.url)
+        return "-"
+    image_tag.short_description = "Rasm"
