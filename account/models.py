638e693550b4d477d67140f3bfef2c3bad047c6b
Dilshoda211 <dilshodabegimyunusova@gmail.com>
2025-09-27 15:41:59 +0500

    change

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=13, unique=True)
+    phone_number = models.CharField(max_length=13, unique=True,blank=True,null=True)
     photo = models.ImageField(upload_to="Images/Users/",blank=True,null=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import BlogCategory, BlogPost, BlogComment
 
-admin.site.register(BlogCategory)
-admin.site.register(BlogPost)
-admin.site.register(BlogComment)
+# BlogCategory admin
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+    ordering = ('-created_at',)
+
+
+# BlogPost admin
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'category', 'created_at', 'image_tag')
+    search_fields = ('title', 'author__username', 'category__name')
+    list_filter = ('created_at', 'category')
+    readonly_fields = ('image_tag',)
+    prepopulated_fields = {"slug": ("title",)}  # avtomatik slug hosil qilish
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="60" height="60" style="object-fit: cover;" />', obj.image.url)
+        return "-"
+    image_tag.short_description = "Rasm"
+
+
+# BlogComment admin
+@admin.register(BlogComment)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'created_at')
+    search_fields = ('user__username', 'post__title', 'content')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
